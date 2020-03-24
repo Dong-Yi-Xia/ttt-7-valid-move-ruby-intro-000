fc9fc31c8349012e4d777322d44b1e559f2a9bbc
Dong Xia <dong7777xia@gmail.com>
2020-03-24 14:06:49 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,14 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if (board[index] == " " || board[index] == "")
+  if (index > 8)
+    return false
+  end
+  
+  if condition
+   (board[index] == " " || board[index] == "")
     return true
   else
     return false
-
   end
 
 end
