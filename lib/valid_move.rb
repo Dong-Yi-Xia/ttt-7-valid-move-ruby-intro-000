87fdc9adabf618207247cf914ab8bb32f70f5ffc
Dong Xia <dong7777xia@gmail.com>
2020-03-24 14:39:55 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,8 +3,8 @@ def valid_move?(board, index)
   index = index.to_i - 1
 
 
-	if position_taken?(board, index) && index.between?(0,8)
-		true
+	if !position_taken?(board, index) && index.between?(0, 8)
+		return true
   else
     return false
   end
